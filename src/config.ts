31272093f65757b84f44746389183b2820ec6283
Julian Beck <ju-fa-beck@t-online.de>
2024-12-25 21:32:10 +0100

    10 posts per page

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ export const SITE: Site = {
   title: "Julian Beck",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
-  postPerPage: 3,
+  postPerPage: 10,
 };
 
 export const LOCALE = ["en-EN"]; // set to [] to use the environment default
